f6bfe5f05462c82dea8f187abf0ba260e6ed1a5a
MaciaKing <maciasalvasalva@gmail.com>
2025-11-24 19:31:20 +0100

    [fix]: cors policy

--- ai_service/main.py
+++ ai_service/main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],          # En producción puedes restringirlo
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
