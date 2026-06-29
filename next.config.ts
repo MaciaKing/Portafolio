8303473cfd738e19a5354d5a741940d5c6281a15
MaciaKing <maciasalvasalva@gmail.com>
2026-06-29 08:09:28 +0200

    [feat]: publish out/ dir for deploy on github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+
+  images: {
+    unoptimized: true,
+  },
+
+  basePath: '/Portafolio',
+  assetPrefix: '/Portafolio/',
 };
 
 export default nextConfig;
